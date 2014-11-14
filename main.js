f8be0c6797a4cbbe38e26597d562869c2407cb93
Sean Usick <sean258@gmail.com>
2014-11-14 13:04:10 -0600

    connection working

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ var Primus = require('primus');
 var primus = Primus.createServer({ port: 8080, transformer: 'websockets' });
 
 primus.on('connection', function(spark) {
-  spark.write('hello world');
+  spark.write('hello world2');
 });
