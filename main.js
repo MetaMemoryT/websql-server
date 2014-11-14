063aa0799c739606bf53e8978da6434ffd7252b1
Sean Usick <sean258@gmail.com>
2014-11-14 12:36:12 -0600

    start

---
+++ main.js
@@ -0,0 +1,7 @@
+var Primus = require('primus');
+
+var primus = Primus.createServer({ port: 8080, transformer: 'websockets' });
+
+primus.on('connection', function(spark) {
+  spark.write('hello world');
+});
